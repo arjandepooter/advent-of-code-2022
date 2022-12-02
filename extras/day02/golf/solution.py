939bde7ee96672e8d07700bcec2e1fbb43e02cc5
Arjan de Pooter <mail@arjandepooter.nl>
2022-12-02 08:49:16 +0100

    Get rid of sys

--- extras/day02/golf/solution.py
+++ extras/day02/golf/solution.py
@@ -1,5 +1,4 @@
-import sys
 p=print
-d=[(ord((c:=m.split(" "))[0])-65,ord(c[1][0])-88) for m in sys.stdin.readlines()]
+d=[(ord((c:=m.split(" "))[0])-65,ord(c[1][0])-88) for m in open(0).readlines()]
 p(sum(1+b+3*((b-a+1)%3) for a,b in d))
 p(sum(3*b+(1+(a+b+2)%3) for a,b in d))
