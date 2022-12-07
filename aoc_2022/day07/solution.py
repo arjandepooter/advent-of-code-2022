c477886884cf821cf8e32b55c274c4eef9cabf0c
Arjan de Pooter <mail@arjandepooter.nl>
2022-12-07 06:37:50 +0100

    Day 7

---
+++ aoc_2022/day07/solution.py
@@ -0,0 +1,76 @@
+Return = int
+
+
+class Dir:
+    name: str
+    dirs: list["Dir"]
+    files: list[tuple[str, int]]
+    parent: "Dir"
+
+    def __init__(self, name: str, parent: "Dir" = None):
+        self.name = name
+        self.parent = parent
+        self.dirs = []
+        self.files = []
+
+    def add_file(self, file: str, size: int):
+        self.files.append((file, size))
+
+    def add_dir(self, dir: "Dir"):
+        self.dirs.append(dir)
+
+    @property
+    def size(self) -> int:
+        return sum(size for _, size in self.files) + sum(dir.size for dir in self.dirs)
+
+    @property
+    def root(self) -> "Dir":
+        if self.parent:
+            return self.parent.root
+        return self
+
+    def __iter__(self):
+        yield self
+        for d in self.dirs:
+            yield from d
+
+    def __repr__(self):
+        parent_name = self.parent.name if self.parent else ""
+        return f"{parent_name}/{self.name}"
+
+
+def parse_data(data: str) -> Dir:
+    cwd = None
+
+    for line in data.splitlines():
+        if line.startswith("$ cd .."):
+            cwd = cwd.parent
+        elif line.startswith("$ cd"):
+            d = Dir(line[5:].strip(), cwd)
+            if cwd:
+                cwd.add_dir(d)
+            cwd = d
+        elif line.startswith("$ ls"):
+            continue
+        else:
+            if line.startswith("dir"):
+                continue
+            size, name = line.split(" ")
+            cwd.add_file(name, int(size))
+
+    return cwd.root
+
+
+def part_1(input: str) -> Return:
+    root = parse_data(input)
+
+    return sum([d.size for d in root if d.size <= 100000])
+
+
+def part_2(input: str) -> Return:
+    root = parse_data(input)
+
+    to_free = 30_000_000 - (70_000_000 - root.size)
+    sorted_dirs = sorted(list(root), key=lambda d: d.size, reverse=True)
+
+    return min([d.size for d in sorted_dirs if d.size >= to_free])
---
+++ aoc_2022/day07/tests.py
@@ -0,0 +1,55 @@
+import pytest
+from .solution import part_1, part_2
+
+# Tuple with sample data, part 1 result, part 2 result. Set result to None to skip
+SAMPLES = [
+    (
+        r"""$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k""",
+        95437,
+        24933642,
+    )
+]
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [(input, expected) for input, expected, _ in SAMPLES],
+)
+def test_sample_part_1(input, expected):
+    if expected is None:
+        return pytest.skip("Part 1 not implemented")
+
+    assert part_1(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [(input, expected) for input, _, expected in SAMPLES],
+)
+def test_sample_part_2(input, expected):
+    if expected is None:
+        return pytest.skip("Part 2 not implemented")
+
+    assert part_2(input) == expected
