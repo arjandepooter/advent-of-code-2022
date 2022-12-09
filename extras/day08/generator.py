c5e77e68b45071aba1069165489d79327ff2f192
Arjan de Pooter <mail@arjandepooter.nl>
2022-12-09 05:54:17 +0100

    -

--- extras/day08/generator.py
+++ extras/day08/generator.py
@@ -4,11 +4,16 @@ import random
 random.seed = 1337
 
 
-def generate():
-    for r in range(2000):
-        c = random.choices(range(10), range(10, 0, -1), k=2000)
+def generate(rows: int, cols: int):
+    for r in range(rows):
+        c = random.choices(range(10), range(10, 0, -1), k=cols)
         print("".join(str(n) for n in c))
 
 
 if __name__ == "__main__":
+    import sys
+
+    rows = int(sys.argv[1])
+    cols = int(sys.argv[2])
+
     generate()
