6b5330ba3cfe93461a6fcf5e11caf0fa271a22a5
Arjan de Pooter <mail@arjandepooter.nl>
2022-12-12 06:52:36 +0100

    Day 12

---
+++ aoc_2022/day12/solution.py
@@ -0,0 +1,82 @@
+from collections import deque
+from itertools import product
+from typing import Iterator
+
+Data = list[list[int]]
+Return = int
+
+
+def parse_data(data: str) -> tuple[Data, tuple[int, int], tuple[int, int]]:
+    grid = []
+    row = []
+    start = end = None
+
+    for r, line in enumerate(data.splitlines()):
+        row = []
+        for c, cur in enumerate(line):
+            if cur == "S":
+                row.append(0)
+                start = (r, c)
+            elif cur == "E":
+                row.append(25)
+                end = (r, c)
+            else:
+                row.append(ord(cur) - ord("a"))
+
+        grid.append(row)
+
+    return grid, start, end
+
+
+def iter_neighbors(grid: Data, pos: tuple[int, int]) -> Iterator[tuple[int, int]]:
+    for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+        r, c = pos[0] + dr, pos[1] + dc
+        if not (
+            r < 0
+            or c < 0
+            or r >= len(grid)
+            or c >= len(grid[0])
+            or grid[pos[0]][pos[1]] + 1 < grid[r][c]
+        ):
+            yield (r, c)
+
+
+def get_path(
+    grid: list[list[int]], start: tuple[int, int], end: tuple[int, int]
+) -> int | None:
+    queue = deque([(0, start)])
+    visited = set()
+
+    while queue:
+        dist, (r, c) = queue.popleft()
+        if (r, c) in visited:
+            continue
+        visited.add((r, c))
+
+        if (r, c) == end:
+            return dist
+
+        for rn, rc in iter_neighbors(grid, (r, c)):
+            queue.append((dist + 1, (rn, rc)))
+
+
+def part_1(input: str) -> Return:
+    grid, cur, end = parse_data(input)
+
+    path = get_path(grid, cur, end)
+
+    return path
+
+
+def part_2(input: str) -> Return:
+    grid, _, end = parse_data(input)
+
+    paths = []
+    for r, c in product(range(len(grid)), range(len(grid[0]))):
+        if grid[r][c] == 0:
+            path = get_path(grid, (r, c), end)
+
+            if path is not None:
+                paths.append(path)
+
+    return min(paths)
---
+++ aoc_2022/day12/tests.py
@@ -0,0 +1,37 @@
+import pytest
+from .solution import part_1, part_2
+
+# Tuple with sample data, part 1 result, part 2 result. Set result to None to skip
+SAMPLES = [
+    (
+        """Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi""",
+        31,
+        29,
+    )
+]
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [(input, expected) for input, expected, _ in SAMPLES],
+)
+def test_sample_part_1(input, expected):
+    if expected is None:
+        return pytest.skip("Part 1 not implemented")
+
+    assert part_1(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [(input, expected) for input, _, expected in SAMPLES],
+)
+def test_sample_part_2(input, expected):
+    if expected is None:
+        return pytest.skip("Part 2 not implemented")
+
+    assert part_2(input) == expected
